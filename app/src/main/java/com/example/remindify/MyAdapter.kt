3ab3d2759c396b856757957afe31ba94194e542a
W9596134 <>
2023-11-26 16:43:37 +0500

    remove on remove on click listener added

--- app/src/main/java/com/example/remindify/MyAdapter.kt
+++ app/src/main/java/com/example/remindify/MyAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.remindify
 
+import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -37,9 +40,30 @@ class MyAdapter(ls: ArrayList<Reminder>, context: Context?) : RecyclerView.Adapt
             .child("Reminders")
 
         holder.itemView.setOnClickListener {
-            ref.child(ls.get(position).id).removeValue()
-            ls.removeAt(position)
-            notifyDataSetChanged()
+            var intent=Intent(context,ViewReminder::class.java)
+            intent.putExtra("title",ls.get(position).reminder.title)
+            intent.putExtra("des",ls.get(position).reminder.description)
+            intent.putExtra("date",ls.get(position).reminder.date)
+            intent.putExtra("time",ls.get(position).reminder.time)
+            intent.putExtra("id",ls.get(position).id)
+            context?.startActivity(intent)
+            
+
+        }
+
+        holder.itemView.setOnLongClickListener {
+            val builder = AlertDialog.Builder(this.context!!)
+            builder.setTitle("Remove Reminder")
+            builder.setMessage("Are you sure you want to remove this reminder?")
+            builder.setPositiveButton("OK"){dialogInterface, which ->
+                ref.child(ls.get(position).id).removeValue()
+                ls.removeAt(position)
+                notifyDataSetChanged()
+            }
+
+            true
+
+
         }
     }
 
---
+++ app/src/main/java/com/example/remindify/ViewReminder.kt
@@ -0,0 +1,14 @@
+package com.example.remindify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewReminder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_reminder)
+        //get data from intent
+        //set data to views
+
+    }
+}
